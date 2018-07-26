2e10ab004c31cbddaf80d6ac160030234b87a20b
vitaly-redkin <v21267@gmail.com>
2018-07-26 23:03:28 +0300

    Google Analytics Support added

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,11 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
+import * as ReactGA from 'react-ga';
 import { App } from './components/app/App';
 import './index.css';
+
+const gaTrackingId: string = process.env.REACT_APP_GA_TRACKING_ID as string;
+ReactGA.initialize(gaTrackingId);
+ReactGA.pageview(window.location.pathname + window.location.search);
 
 ReactDOM.render(<App />, document.getElementById('root'));
